045646f1d57e4f662978f47481d2e1ef88b4af3e
Karem1986 <karem.ortiz@outlook.com>
2021-07-30 14:14:34 +0200

    added again types for maps, implemented useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 // Styles
 import { Wrapper } from './App.styles';
+import { GoogleMap, Marker, InfoWindow, useJsApiLoader } from '@react-google-maps/api';
 
 const App: React.FC = () => {
+const { isLoaded } = useJsApiLoader({
+  id: 'google-map-script',
+  googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY!
+});
+
+//useRef to access the map:
+// const mapRef = React.useRef<google.maps.Map<Element> | null>(null);
+const mapRef = React.useRef<google.maps.Map>();
+
+
   return (
     <Wrapper>
       <p>Start here!</p>
